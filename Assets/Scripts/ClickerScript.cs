e1258fc6928102464fb886c6cb7b9ec23635ce9d
DorShemTov <83244591+DorShemTov@users.noreply.github.com>
2022-05-11 17:47:23 +0300

    Update ClickerScript.cs

--- Assets/Scripts/ClickerScript.cs
+++ Assets/Scripts/ClickerScript.cs
@@ -3,26 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Upgrade { barrel, bucket, hanpan, haybale }
 public class ClickerScript : MonoBehaviour
 {
 
-    [SerializeField] Text eggAmoutTxt;
-    [SerializeField] float eggAmount;
-    [SerializeField] int _happiness;
-    [SerializeField] int eggAdded = 1;
-    [SerializeField] int happinessNeeded = 100;
+    public static ClickerScript Instance;
+    List<Upgrade> ActiveUpgrades = new List<Upgrade>();
+    public int currentEggs = 0, passiveEggs = 0, eggsToAdd = 1;
 
 
-    public void AddEggs()
+
+    public void Awake()
     {
-        eggAmount += eggAdded;
-        eggAmoutTxt.text = eggAmount.ToString();
-
-        if (eggAmount >= (eggAdded * happinessNeeded))
+        if (Instance != null && Instance != this)
         {
-            eggAdded++;
+            Destroy(this);
         }
+        Instance = this;
     }
 
+    public void Update()
+    {
+        //loop every X time 
+        {
+            foreach (var _upgrade in ActiveUpgrades)
+            {
+                switch (_upgrade)
+                {
 
+                    case Upgrade.barrel:
+                        break;
+                    case Upgrade.bucket:
+                        break;
+                    case Upgrade.hanpan:
+                        break;
+                    case Upgrade.haybale:
+                        break;
+                    default:
+                        break;
+
+
+                }
+            }
+        }
+    }
 }
+
