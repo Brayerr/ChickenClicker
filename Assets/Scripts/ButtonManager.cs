efc030c3525bc3bad94ecc9e95943cc6dba912bb
ShaiMaytav <shaimaytav01@gmail.com>
2022-05-11 19:04:05 +0300

    🤷‍♂️

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -12,6 +12,8 @@ public class ButtonManager : MonoBehaviour
     {
         //Activate Effect
         //Print Added Eggs
+        PlayerData.Instance.currentEggs++;
+        Debug.Log("Collected 1 egg");
     }
 
     public void BuyUpgrade(string name)
@@ -21,6 +23,15 @@ public class ButtonManager : MonoBehaviour
         {
             case "Bucket":
                 _upgradeName = UpgradeName.Bucket;
+                break;
+            case "Barrel":
+                _upgradeName = UpgradeName.Barrel;
+                break;
+            case "HenPen":
+                _upgradeName= UpgradeName.HenPen;
+                break;
+            case "HayBale":
+                _upgradeName = UpgradeName.Haybale;
                 break;
             default:
                 break;
@@ -45,12 +56,16 @@ public class ButtonManager : MonoBehaviour
         }
     }
 
-    public void OpenShop()
-    {
 
-    }
-    public void ClsoeShoop()
+    public void ToggleShopMenu()
     {
-
+        if (shopMenu.activeInHierarchy == false)
+        {
+            shopMenu.SetActive(true);
+        }
+        else
+        {
+            shopMenu.SetActive(false);
+        }
     }
 }
