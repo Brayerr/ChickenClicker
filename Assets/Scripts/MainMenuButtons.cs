970043c78009a3dc2a3d6bdc4391034def24aea9
Brayerr <brayer_alon@walla.com>
2022-05-11 18:07:03 +0300

    MainMenu added

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
